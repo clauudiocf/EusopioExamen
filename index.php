bb41a3b39374e6a7e482c219412cd4d81ae033a3
sebas <sebas@DESKTOP-Q59RVPU>
2017-07-03 16:47:20 -0400

    etg

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<head>
-    <title>Eusopio</title>
-</head>
-<body>
-    <div><a href="Publico/index.php">Perfil Publico</a></div>
-    <div><a href="Privado/index.php">Perfil Privado</a></div>
-</body>
-
+<html>
+    <head>
+        <title>Eusopio</title>
+        <link href="Css/css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body id="contenedor">
+        <div><a href="Publico/index.php">Perfil Publico</a></div>
+        <div><a href="Privado/index.php">Perfil Privado</a></div>
+    </body>
+</html>
