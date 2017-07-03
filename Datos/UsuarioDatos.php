84689114ab24a9bad1df5a0182ac826fdf7afe06
cetecom <cetecom@LC08L1727>
2017-07-03 16:38:52 -0400

    avance 3

--- Datos/UsuarioDatos.php
+++ Datos/UsuarioDatos.php
@@ -2,3 +2,16 @@
 
 include '../Entidades/Usuarios.php';
 include 'Conexion.php';
+
+class UsuarioDatos{
+    function insertarUsuarios($user,$pass){
+        $cnn = new Conexion();
+        $con = $cnn->Conectar();
+        
+        $user = new Usuarios();
+        $user->user=$user;
+        $user->pass=  base64_encode($pass);
+        mysql_select_db($con,"eusopio");
+        //* RELLENAR CON PROYECTO https://www.youtube.com/watch?v=2mP4ACi3dIk
+    }
+}
