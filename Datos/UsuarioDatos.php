fbebdd40b4f115c014680dbe1e061126397b248c
cetecom <cetecom@LC08L5713>
2017-07-06 12:42:01 -0400

    avance

--- Datos/UsuarioDatos.php
+++ Datos/UsuarioDatos.php
@@ -1,17 +1,76 @@
 <?php
 
-include '../Entidades/Usuarios.php';
-include 'Conexion.php';
+include "../entidades/usuarios.php";
+include "conexion.php";
 
-class UsuarioDatos{
-    function insertarUsuarios($user,$pass){
-        $cnn = new Conexion();
-        $con = $cnn->Conectar();
-        
-        $user = new Usuarios();
-        $user->user=$user;
-        $user->pass=  base64_encode($pass);
-        mysql_select_db($con,"eusopio");
-        //* RELLENAR CON PROYECTO https://www.youtube.com/watch?v=2mP4ACi3dIk
+class usuariosDatos extends conexion {
+
+	public function __construct(){
+         $usuarios = new usuarios();
     }
+
+	function insertarUsuarios($user,$pass){
+
+		$con = $this->conectar();
+		
+		$usuarios = new usuarios();
+		$usuarios->user=$usuario;
+		$usuarios->pass = base64_encode($pass);
+		mysqli_select_db($con,"formLogin");
+		$sql = "INSERT INTO usuarios (user,pass) VALUES(
+		'".$user->user."',
+		'".$user->pass."'
+		)";
+		if(mysqli_query($con,$sql)){
+			return true;
+		}else{
+			return false;
+		}
+		mysqli_close($con);
+
+	}
+
+    function validar($user,$pass){
+		$con = $this->conectar();
+		$usuarios = new usuarios();
+		$usuarios->user=$user;
+		$usuarios->pass= base64_encode($pass);
+        
+		mysqli_select_db($con,"formLogin");
+        
+		$sql = "SELECT * FROM usuarios WHERE usuario='".$user->user."' and contrasena='".$user->pass."'";
+        $consulta = mysqli_query($con,$sql);
+        $fila = mysqli_fetch_array($consulta);
+        if($fila>0){
+            if($fila["usuario"] == $user->user && $fila["contrasena"]==$user->pass){
+                return $fila;
+            }
+        }
+
+    }
+
+ 
+
+    public function getDatoU($user,$pass){
+		$con = $this->conectar();
+		$usuarios = new usuarios();
+		$usuarios->user=$user;
+		$usuarios->contrasena = base64_encode($pass);
+        
+		mysqli_select_db($con,"formLogin");
+        
+		$sql = "SELECT * FROM usuarios WHERE usuario='".$user->user."' and contrasena='".$user->contrasena."'";
+        $consulta = mysqli_query($con,$sql);
+        $fila = mysqli_fetch_array($consulta);
+        if($fila>0){
+            if($fila["usuario"] == $user->user && $fila["contrasena"]==$user->pass){
+                return json_encode($fila);
+            }
+        }else{
+            return "";
+        }
+
+    }
+
 }
+?>
